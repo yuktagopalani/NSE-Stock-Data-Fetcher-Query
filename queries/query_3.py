bd3915f07e172c5317e99caf13094565ff65bd1a
yuktagopalani <yuktagopalani@gmail.com>
2022-12-24 20:05:44 +0530

    Updated Query3

--- queries/query_3.py
+++ queries/query_3.py
@@ -1,13 +1,27 @@
-import mysql.connector as c
 from mysql.connector import Error
 from appplication_context import cursor, conn
+from utils.convert_table_to_csv import export
+
 try:
 
     if conn.is_connected():
-        cursor = conn.cursor()
         cursor.execute("USE nse_stock_data;")
-        query = "SELECT * FROM stock_data_30_days TAB1 GROUP BY SYMBOL ORDER BY (((SELECT CLOSE FROM stock_data_30_days WHERE TIMESTAMP=(SELECT MAX(TIMESTAMP) FROM stock_data_30_days  WHERE SYMBOL=TAB1.SYMBOL))-(SELECT OPEN FROM stock_data_30_days WHERE TIMESTAMP=(SELECT MIN(TIMESTAMP) FROM stock_data_30_days WHERE SYMBOL=TAB1.SYMBOL))/SELECT OPEN FROM stock_data_30_days WHERE TIMESTAMP=(SELECT MIN(TIMESTAMP) FROM stock_data_30_days WHERE SYMBOL=TAB1.SYMBOL) LIMIT 25;"
-        cursor.execute("CREATE TABLE query3_output AS " + query)
+        cursor.execute("DROP TABLE IF EXISTS q3_tmp;")
+        cursor.execute("CREATE TABLE q3_tmp AS SELECT * FROM stock_data_30_days GROUP BY symbol having timestamp = min(timestamp);")
+        cursor.execute("DROP TABLE IF EXISTS q3_open;")
+        cursor.execute("CREATE TABLE q3_open AS SELECT symbol, open FROM q3_tmp;")
+        cursor.execute("DROP TABLE IF EXISTS q3_tmp;")
+        cursor.execute("CREATE TABLE q3_tmp AS SELECT * FROM stock_data_30_days GROUP BY symbol having timestamp = max(timestamp);")
+        cursor.execute("DROP TABLE IF EXISTS q3_close;")
+        cursor.execute("CREATE TABLE q3_close AS SELECT symbol, close FROM q3_tmp;")
+        cursor.execute("DROP TABLE IF EXISTS q3_tmp;")
+        cursor.execute("CREATE TABLE q3_tmp AS SELECT * FROM q3_open NATURAL JOIN q3_close;")
+        cursor.execute("DROP TABLE IF EXISTS q3_open;")
+        cursor.execute("DROP TABLE IF EXISTS q3_close;")
+        cursor.execute("CREATE TABLE query3_output AS SELECT * FROM q3_tmp ORDER BY ((close - open)/open) LIMIT 25;")
+        cursor.execute("DROP TABLE IF EXISTS q3_tmp;")
+        export('query3_output')
+
 
 except Error as e:
     print(e)
